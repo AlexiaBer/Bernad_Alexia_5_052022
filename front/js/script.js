6baf0b0cb2968d08136b384dc342242aecebffb9
Alexia Bernad <alexia.bernad.2711@gmail.com>
2022-05-14 15:30:06 +0200

    Premières modifications sur le DOM

--- front/js/script.js
+++ front/js/script.js
@@ -1,24 +1,25 @@
-let products = [];
 
-function getProducts () {
-  products = fetch("http://localhost:3000/api/products")
-  .then(function(res) {
-    if (res.ok) {
-      return res.json();
-    }
-  })
-  .catch(function(err) {
-  });
+fetch("http://localhost:3000/api/products")
+  .then( data => data.json())
+  .then( productsList => {      
+    console.log(productsList);
+    for(let product of productsList){
+      let productArticle = new Object (product);
+      let elt = document.getElementById("items");
+      elt.innerHTML = `
+      
+      <a href="./product.html?id=42">
+      <article>
+        <img src="${productArticle.imageUrl}" alt="${productArticle.altTxt}">
+        <h3 class="productName">${productArticle.name}</h3>
+        <p class="productDescription">${productArticle.description}</p>
+      </article>
+    </a>
+`
+for (product[0]; product<productsList.length; product++) {
+}
 }
 
 
-products = getProducts
+  });
 
-
-console.log(getProducts)
-
-
-
-// const productArticle = document.getElementById("items");
-
-// const product = document.createElement("a")
