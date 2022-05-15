39e8493a75edef74402941e19dd86a6527af9a42
Alexia Bernad <alexia.bernad.2711@gmail.com>
2022-05-15 14:06:03 +0200

    Travail sur l'affichage des vignettes produit sur la page index.html

---
+++ front/js/product.js
@@ -0,0 +1,8 @@
+//class Product{                // pour assigner toutes les propriétés du format JSON à la class Product
+   // constructor(product){
+    //  this.id = product.id;               //le this. représente un objet de la class Product.
+     // this.name = product.name;
+     // this.altTxt = product.altTxt;
+     // this.imageUrl = product.imageUrl;
+   // }
+// }
--- front/js/script.js
+++ front/js/script.js
@@ -2,24 +2,20 @@
 fetch("http://localhost:3000/api/products")
   .then( data => data.json())
   .then( productsList => {      
-    console.log(productsList);
-    for(let product of productsList){
-      let productArticle = new Object (product);
-      let elt = document.getElementById("items");
-      elt.innerHTML = `
-      
-      <a href="./product.html?id=42">
-      <article>
-        <img src="${productArticle.imageUrl}" alt="${productArticle.altTxt}">
-        <h3 class="productName">${productArticle.name}</h3>
-        <p class="productDescription">${productArticle.description}</p>
-      </article>
-    </a>
-`
-for (product[0]; product<productsList.length; product++) {
-}
-}
-
-
-  });
-
+    for(let product of productsList){ //boucle qui parcourt le tableau et pour chaque case du tableau créera une variable "product"
+      let productArticle = new Object;
+      document.getElementById("items").innerHTM = `
+                                                <a href="./product.html?id=42">
+                                                  <article>
+                                                    <img src="${productArticle.imageUrl}" alt="${productArticle.altTxt}">
+                                                    <h3 class="productName">${productArticle.name}</h3>
+                                                    <p class="productDescription">${productArticle.description}</p>
+                                                  </article>
+                                                </a>
+                                                `                          
+    };
+    }
+    )
+    //for(let i = O; i<productsList.length; i++) {}    
+    // productsList.forEach(product => parcourir); // cette ligne passe à un autre canapé !! mais toujours pas TOUS les canapés affichés}
+    //setAttribute pour modif les liens href ?? element.setAttribute(<name>, <value> ) 
